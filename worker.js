e45fdf9fe168705d5f98ca5d546d5c367fe0b38a
Luke Salamone <lukesalamone94@gmail.com>
2020-03-05 14:19:44 +0800

    use bitmask for static eval

--- worker.js
+++ worker.js
@@ -2,6 +2,7 @@ const MAX_DEPTH = 4;
 let totalCalcs = 0;
 let timers = {};
 let winnerCache = {};
+let evalCache = {};
 
 onmessage = event => {
     startClock('totalTime');
@@ -69,7 +70,7 @@ function alphabeta(matrix, depth, alpha, beta, isAiTurn, playerBits, opponentBit
         if(this.checkWinner(playerBits, depth)){
             return isAiTurn ? 9999 : -9999;
         } else {
-            return staticEval(matrix);
+            return staticEval(playerBits);
         }
     }
 
@@ -150,131 +151,43 @@ function getSquaresToCheck(matrix, depth){
 
 function staticEval(matrix){
     startClock('staticEval');
-    let a = horizontalScore(matrix) || 0;
-    let b = verticalScore(matrix) || 0;
-    let c = diagonalScore(matrix) || 0;
+
+    if(matrix in evalCache){
+        return evalCache[matrix];
+    }
+
+    let total = 0;
+    let hMask = 3n;
+    let vMask = 32769n;
+    let d1Mask = 65537n;
+    let d2Mask = 16385n;
+
+    total += matchMask(hMask);
+    total += matchMask(d1Mask);
+    total += matchMask(d2Mask);
+
+    while(vMask <= matrix){
+        if((vMask & matrix) === vMask){
+            total++;
+        }
+
+        vMask *= 2n;
+    }
+
+    evalCache[matrix] = total;
 
     stopClock('staticEval');
-    return a + b + c;
+    return total;
 
-    // perform static analysis on the rows of the board
-    function horizontalScore(matrix){
-        let score = 0;
+    function matchMask(mask){
+        let count = 0;
 
-        for(let i=0; i<matrix.length; i++){
-            let current = 0;
-            let streak = 0;
-
-            for(let j=0; j<matrix[i].length; j++){
-                ({current, streak, score} = scoreConsecutive(matrix[i][j], current, streak, score));
-            }
-
-            if(current !== 0){
-                score += current * adjacentBlockScore(streak);
-            }
+        for(let i=0; mask<=matrix; i++, mask*=2n){
+            if(i%15 > 10) continue;
+            if((mask & matrix) === mask) count++;
         }
 
-        return -1 * score;
-    }
-
-    // static analysis on columns
-    function verticalScore(matrix){
-        let score = 0;
-
-        for(let i=0; i<matrix[0].length; i++){
-            let current = 0;
-            let streak = 0;
-
-            for(let j=0; j<matrix.length; j++){
-                ({current, streak, score} = scoreConsecutive(matrix[j][i], current, streak, score));
-            }
-
-            if(current !== 0){
-                score += current * adjacentBlockScore(streak);
-            }
-        }
-
-        return -1 * score;
-    }
-
-    // static analysis on diagonals
-    function diagonalScore(matrix){
-        // return 0;
-
-        let len = matrix.length, score = 0;
-        let res = {d1: {}, d2: {}, d3: {}, d4: {}};
-
-        for(let i=4; i<len; i++){
-
-            // set current and streak to 0 for each diagonal
-            for(let key in res){
-                res[key] = {streak: 0, current: 0, score: 0};
-            }
-
-            for(let j=0; j<=i; j++){
-                let x = i-j;
-                let y = j;
-                res.d1 = process(matrix[i-j][j], res.d1);
-
-                x = len-1-j;
-                y = i-j;
-                res.d2 = process(matrix[len-1-j][i-j], res.d2);
-
-                x = j;
-                y = len-1-i+j;
-                res.d3 = process(matrix[j][len-1-i+j], res.d3);
-
-                x = len-1-i;
-                y = len - 1 - j;
-                res.d4 = process(matrix[len-1-i+j][len-1-j], res.d4);
-            }
-
-            score += res.d1.score + res.d2.score +
-                res.d3.score + res.d4.score;
-        }
-
-        return -1 * score;
-
-        function process(block, obj){
-            return scoreConsecutive(block, obj.current, obj.streak, obj.score);
-        }
-    }
-
-    // score a consecutive group of blocks
-    function scoreConsecutive(block, current, streak, score){
-        if(block !== current){
-            if(current === 0){
-                current = block;
-                streak = 1;
-            } else {
-                score += current * adjacentBlockScore(streak);
-                current = block;
-                streak = 1;
-            }
-        } else {
-            if(block !== 0) streak++;
-        }
-
-        return {
-            'current': current,
-            'streak': streak,
-            'score': score
-        };
-    }
-
-    /** *
-        * score a consecutive group of blocks
-        *   count:  number in a row
-        *
-    */
-    function adjacentBlockScore(count){
-        const scoreMatrix = [0, 2, 4, 8, 16, 32];
-
-        try {
-            return scoreMatrix[count];
-        } catch(e){
-            return -1;
-        }
+        return count;
     }
 }
 
